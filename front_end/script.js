41edbd538422b914d0914c239c389b794978511d
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-04 22:50:00 +0530

    Completed
    - axios form submission
    - form prettified
    - cleared consoles
    - pseudo elements hides

--- front_end/script.js
+++ front_end/script.js
@@ -1,34 +1,43 @@
 const eventSSE = new EventSource("/events");
 
-const chatForm = document.getElementById("chatForm");
-// console.log("chatForm: ", chatForm);
+const chatForm = document.querySelector("#chatForm");
+const msgEle = document.getElementById("myMsg");
 
 chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("submit");
+  const formData = Object.fromEntries(new FormData(chatForm).entries());
+  //   console.log("formData: ", formData);
+  axios({
+    method: "post",
+    url: "/msg",
+    data: formData,
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+
+  msgEle.value = "";
+  msgEle.focus();
 });
 
 const msgContainer = document.getElementById("msgWraper");
 const msgCard = document.getElementById("msgCard");
 
-const usernameEle = document.getElementById("username");
-const roomEle = document.getElementById("room");
-const messageEle = document.getElementById("message");
-
 const onMessageLogic = async (event) => {
-  console.log("event: ", event.data);
-  console.log("event.data: ", event.data);
+  const msgCardClone = msgCard.cloneNode(true);
+
+  const usernameEle = msgCardClone.getElementsByTagName("h5")[0];
+  const roomEle = msgCardClone.getElementsByTagName("span")[0];
+  const messageEle = msgCardClone.getElementsByTagName("p")[0];
 
   const eventData = JSON.parse(event.data);
 
   const { username, room, message } = eventData;
-  console.log("message: ", message);
-  console.log("room,: ", room);
-  console.log("username: ", username);
 
   usernameEle.innerText = username;
-  roomEle.innerText = room;
   messageEle.innerText = message;
+  roomEle.innerText = room;
+
+  msgCard.classList.remove("hidden");
+  msgContainer.prepend(msgCardClone);
 };
 
 eventSSE.onmessage = onMessageLogic;
