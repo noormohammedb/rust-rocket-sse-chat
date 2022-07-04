8ba02077621b291041d6f1ec3cb13f31dd132830
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-04 18:10:12 +0530

    Backend Ready
    - Rocket Installed
    - Routes Configured
    - Message Structs Fields Restriction
    - SSE Configured
    - Tested and working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::response::stream::{Event, EventStream};
+use rocket::serde::{Deserialize, Serialize};
+use rocket::tokio::select;
+use rocket::tokio::sync::broadcast::{channel, error::RecvError, Sender};
+use rocket::Shutdown;
+use rocket::{form::Form, State};
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/test")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate = len(..10))]
+    pub room: String,
+    #[field(validate = len(..20))]
+    pub username: String,
+    pub message: String,
+}
+
+#[post("/msg", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    let _res = queue.send(form.into_inner());
+}
+
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+
+    EventStream! {
+        loop{
+            let msg = select!{
+                msg = rx.recv() => match msg {
+                    Ok(msg )=> msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+
+            };
+            yield Event::json(&msg);
+        }
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(channel::<Message>(1024).0)
+        .mount("/", routes![index, post, events])
 }
