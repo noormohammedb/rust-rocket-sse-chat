8b873fbe22bfde2cc2c49ecf2200f0fae1570554
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-04 19:47:18 +0530

    Cors Enabled
    - Creates a struct for Fairing
    - Implement apropriate method
    - set headers for enable CORS
    - attached to rocket launcher

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
+use rocket::http::Header;
 use rocket::response::stream::{Event, EventStream};
 use rocket::serde::{Deserialize, Serialize};
 use rocket::tokio::select;
 use rocket::tokio::sync::broadcast::{channel, error::RecvError, Sender};
-use rocket::Shutdown;
 use rocket::{form::Form, State};
+use rocket::{Request, Response, Shutdown};
+
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'a>(&self, _req: &'a Request<'_>, _res: &mut Response<'a>) {
+        _res.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        _res.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        _res.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        _res.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[macro_use]
 extern crate rocket;
@@ -51,6 +76,7 @@ async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStrea
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(CORS)
         .manage(channel::<Message>(1024).0)
         .mount("/", routes![index, post, events])
 }
